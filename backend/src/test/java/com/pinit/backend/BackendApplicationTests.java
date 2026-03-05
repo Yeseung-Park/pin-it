823a6309ef6ad4d798c533f489b18a023c9a5f3b
CNS\yeseung <yeseung@lgcns.com>
2026-03-05 17:11:32 +0900

    백엔드 개발환경 구성

---
+++ backend/src/test/java/com/pinit/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

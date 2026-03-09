577f4912bdd077d24c5564aefe805fcbb80b860e
CNS\yeseung <yeseung@lgcns.com>
2026-03-09 17:21:31 +0900

    header 이어서 작업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/common/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
-      <div className="bg-background font-pretendard text-text-main min-h-screen w-full">
+      <div className="font-pretendard text-text-main min-h-screen w-full">
         <Header />
         <main className="p-8">
           <h2>main 화면</h2>
--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,12 +1,12 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from '@radix-ui/react-navigation-menu'
-import { Button } from '../ui/button'
-import { CircleUserRound, LogOut, User } from 'lucide-react'
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+import { Button } from '@/components/ui/button'
+import { LogOut, User } from 'lucide-react'
 export default function Header() {
   return (
     <header className="bg-surface flex h-16 w-full items-center justify-between">
@@ -15,26 +15,38 @@ export default function Header() {
           PIN-IT
         </div>
       </div>
-      <nav>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuContent>
-                <NavigationMenuLink>test</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </nav>
+      <NavigationMenu className="mx-auto max-w-max">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="#"
+              className={navigationMenuTriggerStyle()}
+            >
+              탐색하기
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="#"
+              className={navigationMenuTriggerStyle()}
+            >
+              나의 지도
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="#"
+              className={navigationMenuTriggerStyle()}
+            >
+              랜덤 여행
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <div className="flex items-center gap-4">
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <User className="h-5 w-5" />
-          {/* <CircleUserRound className='h-5 w-5'/> */}
-          <span className="sr-only">마이페이지</span>
-        </Button>
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <LogOut className="h-5 w-5" />
-          <span className="sr-only">로그아웃</span>
+        <Button variant="ghost" size="icon">
+          <User />
+          <span className="sr-only">프로필</span>
         </Button>
       </div>
     </header>
